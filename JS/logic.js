be3bb628c57d706a21438db2fab1df90fe6925ca
Harrison Ellis <hel46@uclive.ac.nz>
2023-06-29 11:07:24 +1200

    first working JS

---
+++ JS/logic.js
@@ -0,0 +1,8 @@
+function onClickChangeBackground(id) {
+    if (id.style.background == "black") {
+        id.style.background = "white";
+    } else {
+        id.style.background = "black"
+    }
+}
+
