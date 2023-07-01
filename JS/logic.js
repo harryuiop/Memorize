60e98d5e3260fae05104c0947d3bc2d13864b34d
Harrison Ellis <hel46@uclive.ac.nz>
2023-07-01 12:32:19 +1200

    selection logic implemented

--- JS/logic.js
+++ JS/logic.js
@@ -1,31 +1,37 @@
 allCards = []
+let started = false
+let cardPairArrayWithImg = [];
 for (i = 1; i < 21; i++) {
     let card = document.getElementById('card' + i);
     allCards.push(card);
 }
 
 function startGameClicked() {
+    if (started === false) {
+        started = true
+        let cardPairArray = getPairs()
+        cardPairArrayWithImg = givePairsAnImage(cardPairArray)
 
-    let cardPairArray = getPairs()
-    cardPairArrayWithImg = givePairsAnImage(cardPairArray)
-
-    for (card of allCards) {
-        flipCardUp(card)
-    }
-
-    const duration = 1000;
-    setTimeout(() => {
-    for (card of allCards) {
-          flipCardDown(card)
+        for (card of allCards) {
+            flipCardUp(card)
         }
-    }, duration);
 
-    /**
-     *  Ideas:
-     *  - run in a while loop that every found pair gets added to a 
-     *    found array, and then when the length of it is 20, game ends.
-    */
+        const duration = 5000;
+        setTimeout(() => {
+        for (card of allCards) {
+            flipCardDown(card)
+            }
+        }, duration);
+    }
+}
 
+function resetGameClicked() {
+    started = false
+    for (card of allCards) {
+        flipCardDown(card)
+    }
+    startGameClicked()
+    
 }
 
 function getPairs(){
@@ -66,39 +72,65 @@ function givePairsAnImage(PairsArray){
 
 let pairHolder = []
 function selectionLogic(card) {
-    if (pairHolder.length === 0) {
-        pairHolder.push(card)
-        flipCardUp(card)
-    } else {
-        if (findCardsImage(card) === findCardsImage(pairHolder[0])) {
+
+    if (started === true) {
+
+        if (pairHolder.length === 0 && !(card.style.background == "rgb(0, 128, 0)") && !(card === pairHolder[0])) {
+            pairHolder.push(card)
             flipCardUp(card)
-            pairHolder = []
+
         } else {
-            flipCardDown(pairHolder[0])
-            flipCardDown(card)
-            pairHolder = []
+
+            if (findCardsImage(card) === findCardsImage(pairHolder[0]) && !(card === pairHolder[0])) {
+                flipCardUp(card)
+                card.style.background = "rgb(0, 128, 0)"
+                pairHolder[0].style.background = "rgb(0, 128, 0)"
+                pairHolder = []
+
+            } else if (!(card.style.background == ("rgb(0, 128, 0)")) && !(card === pairHolder[0])) {
+                flipCardUp(card)
+                card.style.background = "red"
+
+                if (pairHolder.length != 0) {
+                    pairHolder[0].style.background = "red"
+                    const durations = 400;
+
+                    setTimeout(() => {
+                        flipCardDown(pairHolder[0])
+                        flipCardDown(card)
+                        pairHolder = []
+                    }, durations);
+                    
+                }
+            }
         }
     }
 }
 
 function flipCardUp(card) {
-    card.style.background = "rgb(70, 63, 26)"
-    card.style.display = "flex"; // Set display to flex
-    card.style.justifyContent = "center"; // Center horizontally
-    card.style.alignItems = "center"; // Center vertically
-    card.style.color = "white";
-    card.style.fontSize = "40px";
+    const computedStyle = getComputedStyle(card);
+    const backgroundColor = computedStyle.backgroundColor;
 
-    for (pair of cardPairArrayWithImg) {
-        let newHTMLContent = pair[2];
+    if (backgroundColor !== "rgb(0, 128, 0)" && backgroundColor !== "rgb(255, 0, 0)") {
+        card.style.background = "rgb(70, 63, 26)";
+        card.style.display = "flex"; // Set display to flex
+        card.style.justifyContent = "center"; // Center horizontally
+        card.style.alignItems = "center"; // Center vertically
+        card.style.color = "white";
+        card.style.fontSize = "40px";
 
-        for (cards of pair) {
-            if (card === cards) {
-            cards.innerHTML = newHTMLContent;
+        for (pair of cardPairArrayWithImg) {
+            let newHTMLContent = pair[2];
+
+            for (cards of pair) {
+                if (card === cards) {
+                    cards.innerHTML = newHTMLContent;
+                }
             }
         }
     }
 }
+
 
 function flipCardDown(card) {
     card.style.background = "rgb(45, 148, 183)"
